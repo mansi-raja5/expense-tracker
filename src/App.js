f93d130893cee8b8461315ec92440b3d6abb8d70
Mansi Raja <mansi.raja@encora.com>
2021-12-05 23:46:43 +0530

    Initial Static design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-function App() {
+import Expenses from "./components/Expenses/Expenses";
+
+const App = () => {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Ear phones",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "Mobile phone",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: "e3",
+      title: "Flowers",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
+      <Expenses items = {expenses}/>
     </div>
   );
 }
