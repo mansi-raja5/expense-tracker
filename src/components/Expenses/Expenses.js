9c3d7a828bffb44d8c46ba7fc6c67b42e056919d
Mansi Raja <mansi.raja@encora.com>
2021-12-07 01:22:51 +0530

    Display Dynamic Content conditionally - 1

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,20 +14,25 @@ const Expenses = (props) => {
   const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() == filteredYear;
   });
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onFilterExpenseData={filterExpenseDataHandler}
       />
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      {filteredExpenses.length == 0 ? (
+        <p>No Expense Found!</p>
+      ) : (
+        filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))
+      )}
     </Card>
   );
 };
