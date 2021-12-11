7f5e805b56a4bfa95aa10113e9c3f4b8317e610c
Mansi Raja <mansi.raja@encora.com>
2021-12-12 01:43:28 +0530

    Format file and solved some bugs

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,19 @@
-import ExpenseDate from './ExpenseDate';
-import './ExpenseItem.css';
-import Card from '../UI/Card';
-
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
   return (
-    <Card className="expense-item">
-      <div className="expense-item__description">
-        <ExpenseDate date={props.date}/>
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-    </Card>
+    <li>
+      <Card className="expense-item">
+        <div className="expense-item__description">
+          <ExpenseDate date={props.date} />
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
-}
+};
 
 export default ExpenseItem;
